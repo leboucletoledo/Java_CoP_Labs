f297fbf58a7d77f2a6d6fb2eacf1850fa87e7a0c
leboucletoledo <andres_toledo90@hotmail.com>
2022-09-26 22:27:18 -0500

    Add T26 on Polymorphism

--- Tutorial26_Polymorphism/src/App.java
+++ Tutorial26_Polymorphism/src/App.java
@@ -1,5 +1,30 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Plant plant1 = new Plant();
+        Tree tree1 = new Tree();
+
+        /*
+         * Runs the type of object
+         */
+        Plant plant2 = tree1;
+        plant1.grow();
+        plant2.grow();
+        tree1.shedLeaves();
+
+        /*
+         * Can't run plant2.shedLeaves method on plant2,
+         * despite referencing a Tree object
+         * When calling a method, the type of reference is what matters
+         */
+
+        doGrow(tree1);
+    }
+
+    public static void doGrow(Plant plant) {
+        /*
+         * Polymorphism guarantees that it will run the method
+         * from child class
+         */
+        plant.grow();
     }
 }
